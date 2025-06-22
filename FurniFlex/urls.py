295fa304635cc6ed080ac557156cc33a49a9c05f
sandhyaNepal20 <nsandhya192@gmail.com>
2025-06-22 20:57:13 +0545

    First commit

---
+++ FurniFlex/urls.py
@@ -0,0 +1,34 @@
+"""
+URL configuration for FurniFlex project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views  # ✅ This line fixes the error
+
+from FurniFlex.views import home_view
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+        path('', home_view, name='home') , # Use 'home' as the name for the root URL
+        path('cart/', views.cart_view, name='cart'),
+    path('login/', views.login_view, name='login'),
+    path('signup/', views.signup_view, name='signup'),
+    path('reset-password/', views.password_reset_view, name='password_reset'),
+
+
+
+
+]
---
+++ FurniFlex/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render,redirect
+
+
+def home_view(request):
+    return render(request, 'home.html')
+
+
+def cart_view(request):
+    return render(request, 'cart.html')
+def login_view(request):
+    return render(request, 'login.html')
+def signup_view(request):
+    return render(request, 'signup.html')
+def password_reset_view(request):
+    return render(request, 'password_reset.html')  # make sure this template exists
